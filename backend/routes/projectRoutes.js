7ab4bd0cb0b25bc332e41ec0948262c6cbc2f744
Aakash <Avinash@Aakashs-MacBook-Pro.local>
2026-01-21 23:17:22 +0530

    backend route fix

--- backend/routes/projectRoutes.js
+++ backend/routes/projectRoutes.js
@@ -13,7 +13,7 @@ const router = express.Router();
 
 // STUDENT: submit project proposal
 router.post("/create", protect, createProject);
-router.post("/:id/invest", protect, investInProject);
+router.post("/invest/:id", protect, investInProject);
 router.get("/mine", protect, getMyProjects);
 router.get("/marketplace", protect, getMarketplaceProjects);
 router.get("/completed", protect, getCompletedProjects);
